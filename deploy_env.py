676a78abd5ce2273f68c4db414c965d379b85f56
Ajarofcode <103819351+aJarOfCode@users.noreply.github.com>
2024-02-11 15:32:07 +0800

    Update deploy_env.py

--- deploy_env.py
+++ deploy_env.py
@@ -4,48 +4,36 @@ import sys
 import configparser
 
 def read_config():
-    config=configparser.ConfigParser()
+    config = configparser.ConfigParser()
     config.read('config.ini')
     return config
 
-def configuration_parameter():
+def parse_args():
     parser = argparse.ArgumentParser(description="This tool helps you quickly deploy your environment (Python only).")
-    parser.add_argument('-f','--file', type=str, help="Specifies the passed file.")
-    parser.add_argument('-dir','--folder',type=str, help="Specifies the passed folder.")
-    parser.add_argument('--conda',type=bool,default=False,help="Specifies whether to use the conda command.")
+    parser.add_argument('-f', '--file', type=str, help="Specifies the passed file.")
+    parser.add_argument('-d', '--directory', type=str, help="Specifies the passed folder.")
+    parser.add_argument('--conda', type=bool, default=False, help="Specifies whether to use the conda command.")
     args = parser.parse_args()
     return args
-
-args = configuration_parameter()
-
-def Use_requirements():
-    config=read_config()
+args=parse_args()
+def install_requirements(config):
     if args.conda:
-        os.popen("conda install --file "+args.folder).read()
+        os.popen("conda install --file " + args.directory + "requirements.txt -i "+config.get('mirror','conda_mirror')).read()
     else:
-        os.popen("pip install -r "+args.folder).read()
+        os.popen("pip install -r " + args.directory + "requirements.txt"+config.get('mirror','pip_mirror')).read()
+    os.popen().close()
 
-def open_file(file_name):
-    os.popen("pip freeze <"+file_name+"> > requirements.txt")
-    Use_requirements()
+def freeze_requirements(config):
 
-def open_folder():
-    try:
-        files=os.listdir(args.folder)
-        if "requirements.txt" not in files:
-            os.popen("cd "+args.folder).read()
-            os.popen("pip freeze > requirements.txt")
-        Use_requirements()
-    except:
-        print("Sorry,Folder not found.Check that the path is correct.")
-
+    os.popen("pip freeze < " + args.file + " > requirements.txt")
+    install_requirements(config)
 
 def main():
-    args = configuration_parameter()
+    config = read_config()
     if args.file:
-        open_file(args.file)
-    if args.folder:
-        open_folder()
+        freeze_requirements(config)
+    if args.directory:
+        install_requirements(config)
 
 if __name__ == "__main__":
     main()
